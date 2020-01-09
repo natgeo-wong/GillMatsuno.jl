b4e66a2a2f42bb2e16d2cfc5314907db7491d481
natgeo-wong <natgeo.wong@outlook.com>
2020-01-09 17:57:48 -0500

    Rewrote calculations to speed up and prevent unnecessary allocations in loops.

--- src/numdefault.jl
+++ src/numdefault.jl
@@ -12,54 +12,78 @@ This script decomposes the above equations into their numerical form.  We take a
 initial conditions u = v = ϕ = 0.
 """
 
-function ufield(u::Array,ϕ::Array,v::Array,y::Array,
-                δt::Real,δx::Real;parameters::Array=[0.1,0.5,1,1])
+function ufield!(
+    un::Array,u::Array,ϕ::Array,v::Array,y::Array,δt::Real,δx::Real;
+    parameters::Array=[0.1,0.5,1,1],nsize::Array
+)
 
-    α,β,g,H = parameters;
+    α,β,g,H = parameters; nx,ny = nsize;
 
-    ϕ_x = (circshift(ϕ,-1) - circshift(ϕ,1)) / (2*δx);
-    ϕ_x[1,:] .= 0; ϕ_x[end,:] .= 0;
-    # ϕ_x[1,:] .= 3*(ϕ_x[3,:]-ϕ_x[2,:]) - ϕ_x[4,:];
-    # ϕ_x[end,:] .= 3*(ϕ_x[end-3,:]-ϕ_x[end-2,:]) - ϕ_x[end-4,:];
+    for jj = 1 : ny, ii = 1 : nx
 
-    return u - δt * (α*u - β.*y.*v + ϕ_x);
+        if ii != 1 && ii != nx; ϕx = (ϕ[ii+1,jj] - ϕ[ii-1,jj]) / (2*δx)
+        elseif ii == 1;         ϕx = -3*ϕ[ii,jj] + 4*ϕ[ii+1,jj] - ϕ[ii+2,jj];
+        else;                   ϕx =  3*ϕ[ii,jj] - 4*ϕ[ii-1,jj] + ϕ[ii-2,jj];
+        end
 
-end
+        un[ii,jj] = u[ii,jj] - δt * (α*u[ii,jj] - β*y[ii,jj]*v[ii,jj] + ϕx);
 
-function vfield(v::Array,ϕ::Array,u::Array,y::Array,
-                δt::Real,δy::Real;parameters::Array=[0.1,0.5,1,1])
+    end
 
-    α,β,g,H = parameters;
-
-    ϕ_y = (circshift(ϕ,(0,-1)) - circshift(ϕ,(0,1))) / (2*δy);
-    ϕ_y[:,1] .= 0; ϕ_y[:,end] .= 0
-    # ϕ_y[:,1] .= 3*(ϕ_y[:,3]-ϕ_y[:,2]) - ϕ_y[:,4];
-    # ϕ_y[:,end] .= 3*(ϕ_y[:,end-3]-ϕ_y[:,end-2]) - ϕ_y[:,end-4];
-
-    return v - δt * (α*v + β.*y.*u + ϕ_y);
+    return
 
 end
 
-function ϕfield(ϕ::Array,u::Array,v::Array,Q::Array,
-                δt::Real,δx::Real,δy::Real;parameters::Array=[0.1,0.5,1,1])
+function vfield!(
+    vn::Array,v::Array,ϕ::Array,u::Array,y::Array,δt::Real,δy::Real;
+    parameters::Array=[0.1,0.5,1,1],nsize::Array
+)
 
-    α,β,g,H = parameters;
+    α,β,g,H = parameters; nx,ny = nsize;
 
-    v_y = (circshift(v,(0,-1)) - circshift(v,(0,1))) / (2*δy);
-    v_y[:,1] .= 0; v_y[:,end] .= 0
-    # v_y[:,1] .= 3*(v_y[:,3]-v_y[:,2]) - v_y[:,4];
-    # v_y[:,end] .= 3*(v_y[:,end-3]-v_y[:,end-2]) - v_y[:,end-4];
+    for jj = 1 : ny, ii = 1 : nx
 
-    u_x = (circshift(u,-1) - circshift(u,1)) / (2*δx);
-    u_x[1,:] .= 0; u_x[end,:] .= 0;
-    # u_x[1,:] .= 3*(u_x[3,:]-u_x[2,:]) - u_x[4,:];
-    # u_x[end,:] .= 3*(u_x[end-3,:]-u_x[end-2,:]) - u_x[end-4,:];
+        if jj != 1 && jj != ny; ϕy = (ϕ[ii,jj+1] - ϕ[ii,jj-1]) / (2*δy)
+        elseif jj == 1;         ϕy = -3*ϕ[ii,jj] + 4*ϕ[ii,jj+1] - ϕ[ii,jj+2];
+        else;                   ϕy =  3*ϕ[ii,jj] - 4*ϕ[ii,jj-1] + ϕ[ii,jj-2];
+        end
 
-    return ϕ - δt * (α*ϕ + g*H*(u_x+v_y) + Q);
+        vn[ii,jj] = v[ii,jj] - δt * (α*v[ii,jj] + β*y[ii,jj]*u[ii,jj] + ϕy);
+
+    end
+
+    return
 
 end
 
-function Qfield(Q::Array,A::Real,L::Real,xvec::Array,yvec::Array,nx::Integer,ny::Integer)
+function ϕfield!(
+    ϕn::Array,ϕ::Array,u::Array,v::Array,Q::Array,δt::Real,δx::Real,δy::Real;
+    parameters::Array=[0.1,0.5,1,1],nsize::Array
+)
+
+    α,β,g,H = parameters; nx,ny = nsize;
+
+    for jj = 1 : ny, ii = 1 : nx
+
+        if jj != 1 && jj != ny; vy = (v[ii,jj+1] - v[ii,jj-1]) / (2*δy)
+        elseif jj == 1;         vy = -3*v[ii,jj] + 4*v[ii,jj+1] - v[ii,jj+2];
+        else;                   vy =  3*v[ii,jj] - 4*v[ii,jj-1] + v[ii,jj-2];
+        end
+
+        if ii != 1 && ii != nx; ux = (u[ii+1,jj] - u[ii-1,jj]) / (2*δx)
+        elseif ii == 1;         ux = -3*u[ii,jj] + 4*u[ii+1,jj] - u[ii+2,jj];
+        else;                   ux =  3*u[ii,jj] - 4*u[ii-1,jj] + u[ii-2,jj];
+        end
+
+        ϕn[ii,jj] = ϕ[ii,jj] - δt * (α*ϕ[ii,jj] + g*H*(ux+vy) + Q[ii,jj]);
+
+    end
+
+    return
+
+end
+
+function Qfield!(Q::Array,A::Real,L::Real,xvec::Array,yvec::Array,nx::Integer,ny::Integer)
 
     for jj = 1 : ny
         for ii = 1 : nx
@@ -70,31 +94,28 @@ function Qfield(Q::Array,A::Real,L::Real,xvec::Array,yvec::Array,nx::Integer,ny:
         end
     end
 
-    return Q
+    return
 
 end
 
 function GMcalc(;xmin::Real=-25.0,xmax::Real=50.0,δx::Real=0.1,
                 ymin::Real=-10.0,ymax::Real=10.0,δy::Real=0.1,
-                nt::Integer=2000,δt::Real=0.1,
+                nt::Integer=5000,δt::Real=0.001,
                 A::Real=1,L::Real=2,α::Real=0.1,β::Real=0.5,g::Real=1.0,H::Real=1.0)
 
     xvec = convert(Array,xmin:δx:xmax); nx = length(xvec);
     yvec = convert(Array,ymin:δy:ymax); ny = length(yvec);
-    x = repeat(xvec,1,ny);
-    y = convert(Array,transpose(repeat(yvec,1,nx)));
-    Q = zeros(nx,ny); Q = Qfield(Q,A,L,xvec,yvec,nx,ny);
+    x = repeat(xvec,1,ny); y = convert(Array,transpose(repeat(yvec,1,nx)));
+    Q = zeros(nx,ny); Qfield!(Q,A,L,xvec,yvec,nx,ny);
     ϕ = zeros(nx,ny); u = zeros(nx,ny); v = zeros(nx,ny);
+    ϕn = zeros(nx,ny); un = zeros(nx,ny); vn = zeros(nx,ny);
+    nsize = [nx,ny];
 
-    while δt > 0.5 * δx || δt > 0.5 * δy
-        δt = δt / 2;
-    end
-
-    for ii = 1 : nt
-        ϕn = ϕfield(ϕ,u,v,Q,δt,δx,δy,parameters=[α,β,g,H])
-        un = ufield(u,ϕ,v,y,δt,δx,parameters=[α,β,g,H])
-        vn = vfield(v,ϕ,u,y,δt,δy,parameters=[α,β,g,H])
-        ϕ = deepcopy(ϕn); u = deepcopy(un); v = deepcopy(vn);
+    for tt = 1 : nt
+        ϕfield!(ϕn,ϕ,u,v,Q,δt,δx,δy,parameters=[α,β,g,H],nsize=nsize)
+        ufield!(un,u,ϕ,v,y,δt,δx,parameters=[α,β,g,H],nsize=nsize)
+        vfield!(vn,v,ϕ,u,y,δt,δy,parameters=[α,β,g,H],nsize=nsize)
+        ϕ .= ϕn; u .= un; v .= vn;
     end
 
     return ϕ,u,v
